d6a63e52c8283f7f0ece7bf5b28dd4efacbf110e
matsurai <matsurai@souzen.io>
2023-12-24 20:12:59 +0900

    headの記述場所を更新

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,49 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang='ja'>
-      <Head>
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-        />
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin=''
-        ></link>
-        <link
-          href='https://fonts.googleapis.com/css2?family=K2D:wght@300&family=Noto+Sans+JP:wght@400;700&display=swap'
-          rel='stylesheet'
-        ></link>
-        <style>
-          {`html {background:#FFF} body {opacity:0}`}
-        </style>
-        <script
-          async
-          src='https://platform.twitter.com/widgets.js'
-        ></script>
-        <meta
-          property='og:site_name'
-          content={'株式会社souzen'}
-        />
-        <meta
-          property='og:url'
-          content={'https://souzen.io'}
-        />
-        <meta property='og:type' content='website' />
-        <link
-          rel='shortcut icon'
-          href='https://souzen.io/favicon.ico'
-        />
-        <meta name='theme-color' content='#0000FF' />
-        <meta name='twitter:card' content='summary' />
-        <meta
-          property='og:image'
-          content={'https://souzen.io/ogp.webp'}
-        />
-      </Head>
       <body>
         <Main />
         <NextScript />
